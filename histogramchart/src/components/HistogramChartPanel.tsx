a2cfb5642b5321b9b3bf047d89f70fb21a1de229
Guillaume LADORME <Gladorme@users.noreply.github.com>
2025-04-28 16:34:04 +0200

    [FEATURE] Add histogram panel
    Signed-off-by: Guillaume LADORME <Gladorme@users.noreply.github.com>

---
+++ histogramchart/src/components/HistogramChartPanel.tsx
@@ -0,0 +1,99 @@
+// Copyright 2023 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Box, Stack, Typography } from '@mui/material';
+import { TimeSeriesData } from '@perses-dev/core';
+import { PanelProps } from '@perses-dev/plugin-system';
+import merge from 'lodash/merge';
+import { ReactElement, useMemo } from 'react';
+import { useChartsTheme } from '@perses-dev/components';
+import { DEFAULT_FORMAT, HistogramChartOptions } from '../histogram-chart-model';
+import { HistogramChart, HistogramChartData } from './HistogramChart';
+
+const HISTOGRAM_MIN_WIDTH = 90;
+
+export type HistogramChartPanelProps = PanelProps<HistogramChartOptions, TimeSeriesData>;
+
+export function HistogramChartPanel(props: HistogramChartPanelProps): ReactElement | null {
+  const { spec: pluginSpec, contentDimensions, queryResults } = props;
+  const { min, max } = pluginSpec;
+
+  const chartsTheme = useChartsTheme();
+  // ensures all default format properties set if undef
+  const format = merge({}, DEFAULT_FORMAT, pluginSpec.format);
+
+  const histogramData: HistogramChartData[] = useMemo(() => {
+    const buckets: HistogramChartData[] = [];
+
+    for (const result of queryResults) {
+      for (const timeSeries of result.data.series) {
+        if (!timeSeries.histograms) {
+          continue;
+        }
+
+        const [, histoBuckets] = timeSeries.histograms[0];
+        if (histoBuckets) {
+          buckets.push(histoBuckets);
+        }
+      }
+    }
+    return buckets;
+  }, [queryResults]);
+
+  // no data message handled inside chart component
+  if (histogramData.length === 0) {
+    return (
+      <Stack justifyContent="center" height="100%">
+        <Typography variant="body2" color="text.secondary" textAlign="center">
+          No data available (only native histograms are supported for now)
+        </Typography>
+      </Stack>
+    );
+  }
+
+  if (contentDimensions === undefined) return null;
+
+  // accounts for showing a separate chart for each time series
+  let chartWidth = contentDimensions.width / histogramData.length - chartsTheme.container.padding.default;
+  if (chartWidth < HISTOGRAM_MIN_WIDTH && histogramData.length > 1) {
+    // enables horizontal scroll when charts overflow outside of panel
+    chartWidth = HISTOGRAM_MIN_WIDTH;
+  }
+
+  return (
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        // so scrollbar only shows when necessary
+        overflowX: histogramData.length > 1 ? 'scroll' : 'auto',
+      }}
+    >
+      {histogramData.map((series, seriesIndex) => {
+        return (
+          <Box key={`histogram-series-${seriesIndex}`}>
+            <HistogramChart
+              width={chartWidth}
+              height={contentDimensions.height}
+              data={series}
+              format={format}
+              min={min}
+              max={max}
+            />
+          </Box>
+        );
+      })}
+    </Stack>
+  );
+}
