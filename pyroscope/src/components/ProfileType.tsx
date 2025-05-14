e17e1966b4ec3de5d4d6c7c104d07a347bb6f1a9
Steve LENING <steve.lening@amadeus.com>
2025-05-14 10:52:52 +0200

    [FEATURE] Add Pyroscope and FlameChart
    Signed-off-by: Steve LENING <steve.lening@amadeus.com>

---
+++ pyroscope/src/components/ProfileType.tsx
@@ -0,0 +1,82 @@
+// Copyright 2025 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { ReactElement, useEffect, useState } from 'react';
+import { InputLabel, Stack, useTheme, Select, MenuItem } from '@mui/material';
+import { PyroscopeClient } from '../model';
+
+export interface ProfileTypeProps {
+  client: PyroscopeClient | undefined;
+  value: string;
+  onChange?(value: string): void;
+}
+
+async function fetchProfileTypes(client: PyroscopeClient): Promise<Array<{ ID: string; text: string }>> {
+  const response = await client.searchProfileTypes(
+    {}, //param
+    { 'content-type': 'application/json' }, // headers
+    {} // body
+  );
+  return response.profileTypes.map((type) => ({
+    ID: type.ID,
+    text: type.name + '/' + type.sampleType,
+  }));
+}
+
+export function ProfileType(props: ProfileTypeProps): ReactElement {
+  const theme = useTheme();
+  const { client, value, onChange } = props;
+
+  const [options, setOptions] = useState<ReactElement[]>([]);
+
+  // update options when the client changes
+  useEffect(() => {
+    const updateOptions = async () => {
+      if (client) {
+        const profileTypes = await fetchProfileTypes(client);
+        const menuItems = profileTypes.map((type) => (
+          <MenuItem key={type.ID} value={type.ID}>
+            {type.text}
+          </MenuItem>
+        ));
+        setOptions(menuItems);
+      }
+    };
+
+    updateOptions().catch((error) => {
+      console.error('Failed to fetch profile types:', error);
+    });
+  }, [client]);
+
+  return (
+    <Stack position="relative" sx={{ flexGrow: 1 }}>
+      <InputLabel
+        shrink
+        sx={{
+          position: 'absolute',
+          top: '-6px',
+          left: '10px',
+          padding: '0 4px',
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.background.default,
+          zIndex: 1,
+        }}
+      >
+        Profile Type
+      </InputLabel>
+      <Select value={value} size="small" onChange={(event) => onChange?.(event.target.value)}>
+        {options.length > 0 && options}
+      </Select>
+    </Stack>
+  );
+}
