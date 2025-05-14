1fe5af049595a6cd6665bcb26d6b3dd079e031a8
Steve LENING <steve.lening@amadeus.com>
2025-05-14 10:52:52 +0200

    [FEATURE] Add Pyroscope and FlameChart
    Signed-off-by: Steve LENING <steve.lening@amadeus.com>

---
+++ pyroscope/src/components/FilterItem.tsx
@@ -0,0 +1,103 @@
+// Copyright 2025 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { ReactElement, useState, useEffect } from 'react';
+import { Stack } from '@mui/material';
+import { PyroscopeClient } from '../model';
+import { LabelName } from './LabelName';
+import { Operator } from './Operator';
+import { LabelValue } from './LabelValue';
+import { DeleteFilterItem } from './DeleteFilterItem';
+
+export interface FilterItemProps {
+  client: PyroscopeClient | undefined;
+  value: string;
+  onChange?(value: string): void;
+  deleteItem?(): void; // this function is used to delete the current filter
+}
+
+export function FilterItem(props: FilterItemProps): ReactElement {
+  const { client, value, onChange, deleteItem } = props;
+
+  const [labelName, setLabelName] = useState('');
+  const [operator, setOperator] = useState('');
+  const [labelValue, setLabelValue] = useState('');
+
+  // initialize states with the spec if needed
+  useEffect(() => {
+    if (value !== '') {
+      decomposeValue();
+    }
+  }, []);
+
+  // update the filterItem value when one of his children change
+  useEffect(() => {
+    const newValue = labelName + operator + '"' + labelValue + '"';
+    if (labelName !== '' && operator !== '' && labelValue !== '' && newValue !== value) {
+      onChange?.(newValue);
+    }
+  }, [labelName, operator, labelValue]);
+
+  const handleLabelNameChange = (name: string) => {
+    setLabelName(name);
+  };
+
+  const handleOperatorChange = (op: string) => {
+    setOperator(op);
+  };
+
+  const handleLabelValueChange = (value: string) => {
+    setLabelValue(value);
+  };
+
+  const handleDeleteClick = () => {
+    deleteItem?.();
+  };
+
+  const decomposeValue = () => {
+    let sep = '';
+
+    switch (true) {
+      case value.includes('!='):
+        sep = '!=';
+        break;
+      case value.includes('=~'):
+        sep = '=~';
+        break;
+      case value.includes('!~'):
+        sep = '!~';
+        break;
+      case value.includes('='):
+        sep = '=';
+        break;
+      default:
+        console.warn('No valid operator found in value:', value);
+        break;
+    }
+
+    if (sep !== '') {
+      setLabelName(value.split(sep)[0]);
+      setOperator(sep);
+      setLabelValue(value.split(sep)[1].slice(1, -1));
+    }
+  };
+
+  return (
+    <Stack direction="row" spacing={0}>
+      <LabelName client={client} value={labelName} onChange={handleLabelNameChange} />
+      <Operator value={operator} onChange={handleOperatorChange} />
+      <LabelValue client={client} value={labelValue} labelName={labelName} onChange={handleLabelValueChange} />
+      <DeleteFilterItem onClick={handleDeleteClick} />
+    </Stack>
+  );
+}
---
+++ pyroscope/src/plugins/pyroscope-profile-query/get-profile-data.ts
@@ -0,0 +1,182 @@
+// Copyright 2025 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { ProfileQueryPlugin } from '@perses-dev/plugin-system';
+import { AbsoluteTimeRange, StackTrace, ProfileData } from '@perses-dev/core';
+import { getUnixTime } from 'date-fns';
+import { PyroscopeProfileQuerySpec, PYROSCOPE_DATASOURCE_KIND, PyroscopeDatasourceSelector } from '../../model';
+import { PyroscopeClient } from '../../model/pyroscope-client';
+import { SearchProfilesParameters, SearchProfilesResponse } from '../../model/api-types';
+
+export function getUnixTimeRange(timeRange: AbsoluteTimeRange): { start: number; end: number } {
+  const { start, end } = timeRange;
+  return {
+    start: Math.ceil(getUnixTime(start)),
+    end: Math.ceil(getUnixTime(end)),
+  };
+}
+
+export const getProfileData: ProfileQueryPlugin<PyroscopeProfileQuerySpec>['getProfileData'] = async (
+  spec,
+  context
+) => {
+  const defaultPyroscopeDatasource: PyroscopeDatasourceSelector = {
+    kind: PYROSCOPE_DATASOURCE_KIND,
+  };
+
+  const client: PyroscopeClient = await context.datasourceStore.getDatasourceClient(
+    spec.datasource ?? defaultPyroscopeDatasource
+  );
+
+  const makeQuery = () => {
+    let query: string = '';
+    if (spec.service) {
+      query = `service_name="${spec.service}"`;
+    }
+    if (spec.filters && spec.filters.length > 0) {
+      // remove empty filters
+      const newFilters = spec.filters.filter((filter) => filter.value !== '').map((filter) => filter.value);
+      if (query === '') {
+        query = newFilters.join(',');
+      } else {
+        query += ',' + newFilters.join(',');
+      }
+    }
+    query = spec.profileType + (query === '' ? '' : '{' + query + '}');
+    // console.log('Query:', query);
+    return query;
+  };
+
+  const getParams = (): SearchProfilesParameters => {
+    const params: SearchProfilesParameters = {
+      // example of query
+      // query: `process_cpu:cpu:nanoseconds:cpu:nanoseconds{service_name="pyroscope"}`,
+      query: makeQuery(),
+      // the default value is now-1h
+      from: 'now-1h',
+    };
+
+    // handle time range selection from UI drop down (e.g. last 5 minutes, last 1 hour )
+    if (context.absoluteTimeRange) {
+      const { start, end } = getUnixTimeRange(context.absoluteTimeRange);
+      params.from = start;
+      params.until = end;
+    }
+
+    if (spec.maxNodes) {
+      params.maxNodes = spec.maxNodes;
+    }
+
+    return params;
+  };
+
+  const response = await client.searchProfiles(getParams());
+
+  // return a profile data
+  return transformProfileResponse(response);
+};
+
+/**
+ * This function transform the Pyroscope response into the Perses profile format
+ */
+function transformProfileResponse(response: SearchProfilesResponse): ProfileData {
+  const newResponse: ProfileData = {
+    profile: {
+      stackTrace: {} as StackTrace,
+    },
+    numTicks: 0,
+    maxSelf: 0,
+    metadata: {
+      spyName: '',
+      sampleRate: 0,
+      units: '',
+      name: '',
+    },
+    timeline: {
+      startTime: 0,
+      samples: [],
+      durationDelta: 0,
+    },
+  };
+  const stackTraces: StackTrace[][] = [];
+
+  if (!response) {
+    return {} as ProfileData;
+  }
+
+  // Set the stackTrace property
+  for (let i = 0; i < response.flamebearer.levels.length; i++) {
+    let current = 0;
+    const row: StackTrace[] = [];
+    for (let j = 0; j < response.flamebearer.levels[i].length; j += 4) {
+      const temp: StackTrace = {} as StackTrace;
+      temp.id = response.flamebearer.levels[i][j + 3]; // index in names array
+      temp.name = response.flamebearer.names[temp.id];
+      temp.level = i;
+      temp.total = response.flamebearer.levels[i][j + 1];
+      temp.self = response.flamebearer.levels[i][j + 2];
+
+      // start and end
+      current += response.flamebearer.levels[i][j]; // current += offset
+      temp.start = current;
+      current += response.flamebearer.levels[i][j + 1]; // current += total
+      temp.end = current;
+
+      temp.children = [];
+
+      row.push(temp);
+    }
+
+    stackTraces.push(row);
+  }
+
+  addChildren(stackTraces); // adding children to nodes
+  newResponse.profile.stackTrace = stackTraces[0][0];
+
+  // Set other properties
+  newResponse.numTicks = response.flamebearer.numTicks;
+  newResponse.maxSelf = response.flamebearer.maxSelf;
+
+  newResponse.metadata = {
+    spyName: response.metadata.spyName,
+    sampleRate: response.metadata.sampleRate,
+    units: response.metadata.units,
+    name: response.metadata.name,
+  };
+
+  newResponse.timeline = {
+    startTime: response.timeline.startTime,
+    samples: response.timeline.samples,
+    durationDelta: response.timeline.durationDelta,
+  };
+
+  return newResponse;
+}
+
+// todo: optimize this method as soon as possible
+function addChildren(stackTraces: StackTrace[][]): void {
+  // for (let i = stackTraces.length - 1; i > 0; i--) {
+  for (let i = 1; i < stackTraces.length; i++) {
+    for (let j = 0; j < stackTraces[i].length; j++) {
+      for (let k = 0; k < stackTraces[i - 1].length; k++) {
+        if (
+          stackTraces[i][j].start >= stackTraces[i - 1][k].start &&
+          stackTraces[i][j].end <= stackTraces[i - 1][k].end
+        ) {
+          stackTraces[i - 1][k].children.push(stackTraces[i][j]);
+          break;
+        }
+      }
+    }
+  }
+}
