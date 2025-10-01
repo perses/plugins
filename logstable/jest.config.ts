4588f4a6571fa4fe17465b4acf864d7d76e17d17
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2025-10-01 15:19:41 +0300

    extract LogsTable to a separate plugin
    Signed-off-by: Andrii Chubatiuk <andrew.chubatiuk@gmail.com>

--- logstable/jest.config.ts
+++ logstable/jest.config.ts
@@ -1,4 +1,4 @@
-// Copyright 2025 The Perses Authors
+// Copyright 2023 The Perses Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,13 +11,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { LogEntry, LogData, TimeSeriesData } from '@perses-dev/core';
+import type { Config } from '@jest/types';
+import shared from '../jest.shared';
 
-export interface ClickHouseTimeSeriesData extends TimeSeriesData {
-  logs?: LogData;
-}
+const jestConfig: Config.InitialOptions = {
+  ...shared,
 
-export interface TimeSeriesEntry {
-  time: string;
-  log_count: number | string;
-}
+  setupFilesAfterEnv: [...(shared.setupFilesAfterEnv ?? []), '<rootDir>/src/setup-tests.ts'],
+};
+
+export default jestConfig;
---
+++ logstable/src/test/mock-query-results.ts
@@ -0,0 +1,62 @@
+// Copyright 2023 The Perses Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { LogsQueryData } from '../model';
+
+export const MOCK_LOGS_QUERY_RESULT: LogsQueryData = {
+  logs: {
+    hasMore: false,
+    timeRange: {
+      start: new Date(1666625490000),
+      end: new Date(1666625535000),
+    },
+    entries: [
+      {
+        timestamp: 1666625490000,
+        line: 'foo',
+        labels: {
+          device: '/dev/vda1',
+          env: 'demo',
+          fstype: 'ext4',
+          instance: 'demo.do.prometheus.io:9100',
+          job: 'node',
+          mountpoint: '/',
+        },
+      },
+      {
+        timestamp: 1666625490001,
+        line: 'bar',
+        labels: {
+          device: '/dev/vda15',
+          env: 'demo',
+          fstype: 'vfat',
+          instance: 'demo.do.prometheus.io:9100',
+          job: 'node',
+          mountpoint: '/boot/efi',
+        },
+      },
+    ],
+  },
+};
+
+export const MOCK_LOGS_QUERY_DEFINITION = {
+  kind: 'LogsQuery',
+  spec: {
+    plugin: {
+      kind: 'LokiLogsQuery',
+      spec: {
+        query: '',
+      },
+    },
+  },
+};
