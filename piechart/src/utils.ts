3fc393abb1557bad19895c2ded661a493e1e2736
Adrian Sepiół <adisepko@gmail.com>
2026-03-09 13:46:30 +0100

    [BUGFIX] PieChart: Decimal, Unit & Short Values attributes not taken into account
    Signed-off-by: Adrian Sepiół <adisepko@gmail.com>

--- piechart/src/utils.ts
+++ piechart/src/utils.ts
@@ -11,22 +11,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { SortOption } from '@perses-dev/components';
-import { DEFAULT_SORT } from './pie-chart-model';
+import { LegendItem, ModeOption, SortOption, TableColumnConfig } from '@perses-dev/components';
+import { FormatOptions, formatValue } from '@perses-dev/core';
+import { format } from 'echarts';
+import { comparisonLegends, ComparisonValues } from '@perses-dev/plugin-system';
 import { PieChartData } from './PieChartBase';
+import { DEFAULT_SORT } from './pie-chart-model';
 
-export function calculatePercentages(data: PieChartData[]): Array<{ id?: string; name: string; value: number }> {
-  const sum = data.reduce((accumulator, { value }) => accumulator + (value ?? 0), 0);
-  return data.map((seriesData) => {
-    const percentage = ((seriesData.value ?? 0) / sum) * 100;
-    return {
-      ...seriesData,
-      value: Number(percentage.toFixed(2)),
-    };
-  });
-}
-
-export function sortSeriesData(data: PieChartData[], sortOrder: SortOption = DEFAULT_SORT): PieChartData[] {
+export function sortSeriesData<T extends PieChartData>(data: T[], sortOrder: SortOption = DEFAULT_SORT): T[] {
   return data.sort((a, b) => {
     // Handle null values - push them to the end regardless of sort order
     if (a.value === null && b.value === null) return 0;
@@ -36,5 +28,119 @@ export function sortSeriesData(data: PieChartData[], sortOrder: SortOption = DEF
     // Sort by value
     const diff = (a.value ?? 0) - (b.value ?? 0);
     return sortOrder === 'asc' ? diff : -diff;
+  });
+}
+
+type formatterProps = { name: string; value: number | unknown[] | object; percent: number };
+export const getTooltipFormatter = (formatOptions?: FormatOptions): ((props: formatterProps) => string) => {
+  const relativeFormatOptions = { unit: 'percent', decimalPlaces: formatOptions?.decimalPlaces } as const;
+  return ({ name, value, percent }: formatterProps): string => {
+    if (typeof value === 'number') {
+      return `${format.encodeHTML(name)}: ${formatValue(value, formatOptions)} (${formatValue(percent, relativeFormatOptions)}%)`;
+    }
+    return `${format.encodeHTML(name)}: ${format.encodeHTML(value.toString())}`;
+  };
+};
+export const getLabelFormatter = (
+  mode?: ModeOption,
+  formatOptions?: FormatOptions
+): ((props: formatterProps) => string) => {
+  if (mode === 'percentage') {
+    return percentageLabelFormatter(formatOptions);
+  }
+  return labelFormatter(formatOptions);
+};
+
+const labelFormatter = (formatOptions?: FormatOptions) => {
+  return ({ name, value }: formatterProps): string => {
+    if (typeof value === 'number') {
+      return `${name}:\n${formatValue(value, formatOptions)}`;
+    }
+    return `${name}:\n${format.encodeHTML(value.toString())}`;
+  };
+};
+
+const percentageLabelFormatter = (formatOptions?: FormatOptions) => {
+  const relativeFormatOptions = { unit: 'percent', decimalPlaces: formatOptions?.decimalPlaces } as const;
+  return ({ name, percent }: formatterProps): string => {
+    return `${name}:\n${formatValue(percent, relativeFormatOptions)}`;
+  };
+};
+
+export interface PieChartLegendMapper {
+  mapToLegendItems: (pieChartData: Array<Required<PieChartData>>, selectedValues?: ComparisonValues[]) => LegendItem[];
+  mapToLegendColumns: (
+    selectedValues?: ComparisonValues[],
+    formatOptions?: FormatOptions
+  ) => Array<TableColumnConfig<LegendItem>>;
+}
+
+export class PieChartListLegendMapper implements PieChartLegendMapper {
+  mapToLegendItems(pieChartData: Array<Required<PieChartData>>): LegendItem[] {
+    return pieChartData.map(({ id, name, itemStyle }) => ({ id: id, label: name, color: itemStyle.color, data: {} }));
+  }
+  mapToLegendColumns(): Array<TableColumnConfig<LegendItem>> {
+    return [];
+  }
+}
+
+export class PieChartTableLegendMapper implements PieChartLegendMapper {
+  mapToLegendItems(pieChartData: Array<Required<PieChartData>>, selectedValues?: ComparisonValues[]): LegendItem[] {
+    const relativePieChartData = calculatePercentages(pieChartData);
+    const absoluteValueSelected = selectedValues?.includes('abs');
+    const relativeValueSelected = selectedValues?.includes('relative');
+    return pieChartData.map(({ id, name, itemStyle, value }) => {
+      const data: { [k in ComparisonValues]?: number } = {};
+      if (absoluteValueSelected && typeof value === 'number') {
+        data['abs'] = value;
+      }
+      if (relativeValueSelected) {
+        const itemPercentageValue = relativePieChartData.find((rpd) => rpd.id === id)?.value;
+        if (typeof itemPercentageValue === 'number') {
+          data['relative'] = itemPercentageValue;
+        }
+      }
+      return {
+        id: id,
+        label: name,
+        color: itemStyle.color,
+        data: data,
+      };
+    });
+  }
+
+  mapToLegendColumns(
+    selectedValues?: ComparisonValues[],
+    formatOptions?: FormatOptions
+  ): Array<TableColumnConfig<LegendItem>> {
+    const relativeFormatOptions = { unit: 'percent', decimalPlaces: formatOptions?.decimalPlaces } as const;
+    return (
+      selectedValues?.toSorted().map((v) => ({
+        accessorKey: `data.${v}`,
+        header: comparisonLegends[v]?.label || v,
+        headerDescription: comparisonLegends[v]?.description,
+        width: 90,
+        align: 'right',
+        cellDescription: true,
+        enableSorting: true,
+        cell: ({ getValue }): string => {
+          const cellValue = getValue();
+          return typeof cellValue === 'number' && formatOptions
+            ? formatValue(cellValue, v === 'relative' ? relativeFormatOptions : formatOptions)
+            : cellValue;
+        },
+      })) ?? []
+    );
+  }
+}
+
+function calculatePercentages<T extends PieChartData>(data: T[]): T[] {
+  const sum = data.reduce((accumulator, { value }) => accumulator + (value ?? 0), 0);
+  return data.map((seriesData) => {
+    const percentage = ((seriesData.value ?? 0) / sum) * 100;
+    return {
+      ...seriesData,
+      value: Number(percentage.toFixed(4)),
+    };
   });
 }
