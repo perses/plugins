0fd1acde069ca2032fb363fd7e76e4f10d4490c6
Guillaume LADORME <Gladorme@users.noreply.github.com>
2026-04-20 17:06:04 +0200

    WIP: Prometheus Annotations plugin
    Signed-off-by: Guillaume LADORME <Gladorme@users.noreply.github.com>

---
+++ prometheus/src/components/annotations/PrometheusPromQLAnnotation.tsx
@@ -0,0 +1,16 @@
+import { AnnotationPlugin, AnnotationQueryQueryPluginDependencies, parseVariables } from '@perses-dev/plugin-system';
+import { PrometheusPromQLAnnotationOptions } from '../../plugins';
+import { PrometheusPromQLAnnotationOptionEditor } from './PrometheusPromQLAnnotationOptionEditor';
+import { getAnnotationData } from './get-annotation-data';
+
+export const PrometheusPromQLAnnotation: AnnotationPlugin<PrometheusPromQLAnnotationOptions> = {
+  getAnnotationData: getAnnotationData,
+  dependsOn: (spec: PrometheusPromQLAnnotationOptions): AnnotationQueryQueryPluginDependencies => {
+    const queryVariables = parseVariables(spec.expr);
+    return {
+      variables: [...queryVariables],
+    };
+  },
+  OptionsEditorComponent: PrometheusPromQLAnnotationOptionEditor,
+  createInitialOptions: () => ({ expr: '' }),
+};
