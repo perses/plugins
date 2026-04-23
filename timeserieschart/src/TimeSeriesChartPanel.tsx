59cba3916b3782b38865279ee2a6f2cb66266eed
Guillaume LADORME <Gladorme@users.noreply.github.com>
2026-04-23 17:14:24 +0200

    Fix context singleton
    Signed-off-by: Guillaume LADORME <Gladorme@users.noreply.github.com>

--- timeserieschart/src/TimeSeriesChartPanel.tsx
+++ timeserieschart/src/TimeSeriesChartPanel.tsx
@@ -155,12 +155,15 @@ export function TimeSeriesChartPanel(props: TimeSeriesChartProps): ReactElement 
   const [legendSorting, setLegendSorting] = useState<NonNullable<LegendProps['tableProps']>['sorting']>();
 
   const { setTimeRange } = useTimeRange();
+
   const annotationsWithData = useAnnotationsWithData();
+  console.log('annotationsWithData', annotationsWithData);
+
   const annotations: TimeSeriesAnnotation[] = useMemo(
     () => convertAnnotationToTimeSeriesAnnotation(annotationsWithData),
     [annotationsWithData]
   );
-  console.log(annotations);
+  console.log('annotations', annotations);
 
   // Populate series data based on query results
   const {
