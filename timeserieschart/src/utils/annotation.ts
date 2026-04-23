5806bbc24f3c3c4ce78b9669a75cfbbcc53fbb80
Guillaume LADORME <Gladorme@users.noreply.github.com>
2026-04-23 12:54:19 +0200

    Move from annotation def to spec + add cue schema
    Signed-off-by: Guillaume LADORME <Gladorme@users.noreply.github.com>

--- timeserieschart/src/utils/annotation.ts
+++ timeserieschart/src/utils/annotation.ts
@@ -1,16 +1,14 @@
 import { AnnotationData, AnnotationDisplay } from '@perses-dev/spec';
-import { AnnotationDefinitionWithData } from '@perses-dev/dashboards';
+import { AnnotationSpecWithData } from '@perses-dev/dashboards';
 
 export type TimeSeriesAnnotation = AnnotationData & AnnotationDisplay;
 
-export function convertAnnotationToTimeSeriesAnnotation(
-  annotations: AnnotationDefinitionWithData[]
-): TimeSeriesAnnotation[] {
+export function convertAnnotationToTimeSeriesAnnotation(annotations: AnnotationSpecWithData[]): TimeSeriesAnnotation[] {
   const result: TimeSeriesAnnotation[] = [];
   for (const annotation of annotations) {
     for (const item of annotation.data) {
       result.push({
-        ...annotation.definition.spec.display,
+        ...annotation.definition.display,
         ...item,
       });
     }
