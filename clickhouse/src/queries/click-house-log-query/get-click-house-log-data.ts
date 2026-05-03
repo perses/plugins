15fed8f2a90a78700f596d0465f8d900bc1c7da1
Kostia R <k-bx@k-bx.com>
2026-05-03 22:09:01 +0000

    [BUGFIX] Fix ClickHouse time range interpolation and time series rendering
    Signed-off-by: Kostia R <k-bx@k-bx.com>

--- clickhouse/src/queries/click-house-log-query/get-click-house-log-data.ts
+++ clickhouse/src/queries/click-house-log-query/get-click-house-log-data.ts
@@ -13,7 +13,12 @@
 
 import { replaceVariables, LogQueryPlugin } from '@perses-dev/plugin-system';
 import { LogData, LogEntry } from '@perses-dev/spec';
-import { ClickHouseClient, ClickHouseQueryResponse } from '../../model/click-house-client';
+import {
+  ClickHouseClient,
+  ClickHouseQueryResponse,
+  formatClickHouseDateTime,
+  replaceTimeRangePlaceholders,
+} from '../../model/click-house-client';
 import { DEFAULT_DATASOURCE } from '../constants';
 import { ClickHouseLogQuerySpec } from './click-house-log-query-types';
 
@@ -82,18 +87,21 @@ export const getClickHouseLogData: LogQueryPlugin<ClickHouseLogQuerySpec>['getLo
   )) as ClickHouseClient;
 
   const { start, end } = context.timeRange;
+  const startTime = formatClickHouseDateTime(start);
+  const endTime = formatClickHouseDateTime(end);
+  const executedQueryString = replaceTimeRangePlaceholders(query, startTime, endTime);
 
   const response: ClickHouseQueryResponse = await client.query({
-    start: start.getTime().toString(),
-    end: end.getTime().toString(),
-    query,
+    start: startTime,
+    end: endTime,
+    query: executedQueryString,
   });
 
   return {
     timeRange: { start, end },
     logs: convertStreamsToLogs(response.data as LogEntry[]),
     metadata: {
-      executedQueryString: query,
+      executedQueryString,
     },
   };
 };
