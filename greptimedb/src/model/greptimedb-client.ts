2022942c72de67e6dfdafbe25aee82de554f7749
sun <sunchang_long@163.com>
2026-05-21 10:38:08 +0800

    refactor: use perses fetch
    Signed-off-by: sun <sunchang_long@163.com>

--- greptimedb/src/model/greptimedb-client.ts
+++ greptimedb/src/model/greptimedb-client.ts
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { RequestHeaders } from '@perses-dev/core';
+import { fetch, RequestHeaders } from '@perses-dev/core';
 import { GreptimeDBDatasourceResponse, GreptimeDBQueryRequestParameters } from '../datasources/greptimedb-datasource';
 import { GreptimeDBResponseData } from './greptimedb-data-types';
 
